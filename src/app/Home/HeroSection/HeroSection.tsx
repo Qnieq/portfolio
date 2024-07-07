ffbfda10d8a24a6ce0745046821d1c521ec396c4
Mike <misha.o.son@gmail.com>
2024-07-07 22:28:19 +0300

    added adaptive and animation in hero section

--- src/app/Home/HeroSection/HeroSection.tsx
+++ src/app/Home/HeroSection/HeroSection.tsx
@@ -1,47 +1,270 @@
 'use client'
 import Image from "next/image";
+import { motion } from "framer-motion"
 import styles from "./HeroSection.module.scss"
+import { useEffect, useState } from "react";
 
-const posters: string[] = ["/images/posters/young-dev.svg", "/images/posters/user-exp.svg", "/images/posters/modern-tech.svg", "/images/posters/anim.svg"]
+const posters: string[] = [
+    "/images/posters/young-dev.svg",
+    "/images/posters/user-exp.svg",
+    "/images/posters/modern-tech.svg",
+    "/images/posters/anim.svg",
+    "/images/posters/comm-exp.svg",
+    "/images/posters/cllear-code.svg"
+]
 
 const HeroSection = () => {
+    const [width, setWidth] = useState<number>(window.innerWidth)
+
     const renderSquares = () => {
         const squares = [];
-        for (let i = 0; i < 400; i++) {
+        for (let i = 0; i < 1500; i++) {
             squares.push(<div key={i} className={styles.square}></div>);
         }
         return squares;
     };
+
+    useEffect(() => {
+        window.addEventListener("resize", () => setWidth((current) => (current * 0) + window.innerWidth))
+
+        return () => {
+            window.removeEventListener("resize", () => setWidth((current) => (current * 0) + window.innerWidth))
+        }
+    }, [])
     return (
         <section className={styles.hero_section}>
             <div className={styles.title_container}>
-                <div className={styles.title}>
-                    FRONTEND DEVELOPER
+                <div style={{ overflow: "hidden" }}>
+                    <motion.h1
+                        initial={{
+                            translateY: 200,
+                        }}
+                        animate={{
+                            translateY: 0,
+                        }}
+                        transition={{
+                            delay: 0.7,
+                            ease: "linear",
+                            duration: 1.4,
+                            type: "spring",
+                            bounce: 0.05
+                        }}
+                        className={styles.title}
+                    >
+                        FRONTEND DEVELOPER
+                    </motion.h1>
                 </div>
                 <div className={styles.description_box}>
-                    <p className={styles.desc}>
+                    <motion.p
+                        initial={{
+                            translateY: 200,
+                        }}
+                        animate={{
+                            translateY: 0,
+                        }}
+                        transition={{
+                            delay: 1.5,
+                            ease: "linear",
+                            duration: 1.3,
+                            type: "spring",
+                            bounce: 0.05
+                        }}
+                        className={styles.desc}
+                    >
                         Lorem ipsum dolor, sit amet consectetur adipisicing elit. Velit quo sapiente facilis laudantium ea illo voluptatum fugit sed ipsum. Rerum, dolores sequi?
-                    </p>
-                    <p className={styles.desc}>
+                    </motion.p>
+                    <motion.p
+                        initial={{
+                            translateY: 200,
+                        }}
+                        animate={{
+                            translateY: 0,
+                        }}
+                        transition={{
+                            delay: 2,
+                            ease: "linear",
+                            duration: 1.3,
+                            type: "spring",
+                            bounce: 0.05
+                        }}
+                        className={styles.desc}
+                    >
                         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Perferendis quos sed illum alias error dolores culpa hic tempore nobis explicabo?
-                    </p>
-                    <a href="#" className={styles.contact}>
+                    </motion.p>
+                    <motion.a
+                        initial={{
+                            translateY: 100,
+                        }}
+                        animate={{
+                            translateY: 0,
+                        }}
+                        transition={{
+                            delay: 2.4,
+                            ease: "linear",
+                            duration: 1.3,
+                            type: "spring",
+                            bounce: 0.05
+                        }}
+                        href="#"
+                        className={styles.contact}
+                    >
                         СВЯЗАТЬСЯ
                         <Image width={20} height={20} src={"/icons/arrow-right.svg"} alt="arrow" />
-                    </a>
+                    </motion.a>
                 </div>
             </div>
-            <div className={styles.content}>
-                <div className={styles.bg_element}>
+            <motion.div
+
+                className={styles.content}
+            >
+                <motion.div
+                    initial={{
+                        translateX: -width - 500,
+                    }}
+                    animate={{
+                        translateX: 0,
+                    }}
+                    transition={{
+                        delay: 3.1,
+                        ease: "linear",
+                        duration: 1.5,
+                        type: "spring",
+                        bounce: 0.05
+                    }}
+                    className={styles.bg_element}
+                >
                     <Image width={400} height={750} src={"/images/photo/selfy.png"} alt="" className={styles.photo} />
                     <div className={styles.squares_box}>
                         {renderSquares()}
                     </div>
-                </div>
-                {posters.map((poster, index) => (
-                    <Image key={index} width={300} height={350} src={poster} alt="" className={styles.poster} />
-                ))}
-            </div>
+                </motion.div>
+
+                {/* posters */}
+
+                <motion.div
+                    initial={{
+                        left: -width - 500,
+                        rotate: -120
+                    }}
+                    animate={{
+                        left: "50%",
+                        rotate: 0
+                    }}
+                    transition={{
+                        delay: 3.3,
+                        ease: "linear",
+                        duration: 1.5,
+                        type: "spring",
+                        bounce: 0.05
+                    }}
+                    className={styles.poster_box}
+                >
+                    <Image width={300} height={350} src={"/images/posters/young-dev.svg"} alt="" className={styles.poster} />
+                </motion.div>
+                <motion.div
+                    initial={{
+                        left: -width - 500,
+                        rotate: 120
+                    }}
+                    animate={{
+                        left: "60%",
+                        rotate: 0
+                    }}
+                    transition={{
+                        delay: 3.3,
+                        ease: "linear",
+                        duration: 1.5,
+                        type: "spring",
+                        bounce: 0.05
+                    }}
+                    className={styles.poster_box}
+                >
+                    <Image width={300} height={350} src={"/images/posters/user-exp.svg"} alt="" className={styles.poster} />
+                </motion.div>
+                <motion.div
+                    initial={{
+                        left: -width - 500,
+                        rotate: 130
+                    }}
+                    animate={{
+                        left: "auto",
+                        right: "70%",
+                        rotate: 0
+                    }}
+                    transition={{
+                        delay: 3.3,
+                        ease: "linear",
+                        duration: 1.5,
+                        type: "spring",
+                        bounce: 0.05
+                    }}
+                    className={styles.poster_box}
+                >
+                    <Image width={300} height={350} src={"/images/posters/modern-tech.svg"} alt="" className={styles.poster} />
+                </motion.div>
+                <motion.div
+                    initial={{
+                        left: -width - 500,
+                        rotate: -100
+                    }}
+                    animate={{
+                        left: "auto",
+                        right: "70%",
+                        rotate: 0
+                    }}
+                    transition={{
+                        delay: 3.4,
+                        ease: "linear",
+                        duration: 1.5,
+                        type: "spring",
+                        bounce: 0.05
+                    }}
+                    className={styles.poster_box}
+                >
+                    <Image width={300} height={350} src={"/images/posters/anim.svg"} alt="" className={styles.poster} />
+                </motion.div>
+                <motion.div
+                    initial={{
+                        left: -width - 500,
+                        rotate: 160
+                    }}
+                    animate={{
+                        left: "5%",
+                        rotate: 0
+                    }}
+                    transition={{
+                        delay: 3.5,
+                        ease: "linear",
+                        duration: 1.5,
+                        type: "spring",
+                        bounce: 0.05
+                    }}
+                    className={styles.poster_box}
+                >
+                    <Image width={300} height={350} src={"/images/posters/comm-exp.svg"} alt="" className={styles.poster} />
+                </motion.div>
+                <motion.div
+                    initial={{
+                        left: -width - 500,
+                        rotate: 140
+                    }}
+                    animate={{
+                        left: "auto",
+                        right: "10%",
+                        rotate: 0
+                    }}
+                    transition={{
+                        delay: 3.2,
+                        ease: "linear",
+                        duration: 2,
+                        type: "spring",
+                        bounce: 0.05
+                    }}
+                    className={styles.poster_box}
+                >
+                    <Image width={300} height={350} src={"/images/posters/cllear-code.svg"} alt="" className={styles.poster} />
+                </motion.div>
+            </motion.div>
         </section>
     );
 }
--- src/components/shared/Header/Header.tsx
+++ src/components/shared/Header/Header.tsx
@@ -1,11 +1,54 @@
 "use client"
 import Link from "next/link";
 import styles from "./Header.module.scss"
+import { useEffect, useState } from "react";
+import Image from "next/image";
+import { motion } from "framer-motion"
+
+const navigation: { link: string, title: string }[] = [
+    {
+        link: "#",
+        title: "Work"
+    },
+    {
+        link: "#",
+        title: "About"
+    },
+    {
+        link: "#",
+        title: "Contact"
+    },
+];
 
 const Header = () => {
 
+    const [width, setWidth] = useState<number>(window.innerWidth)
+    const [menuIsOpen, setMenuIsOpen] = useState<boolean>(false)
+
+    useEffect(() => {
+        window.addEventListener("resize", () => setWidth((current) => (current * 0) + window.innerWidth))
+
+        return () => {
+            window.removeEventListener("resize", () => setWidth((current) => (current * 0) + window.innerWidth))
+        }
+    }, [])
+
     return (
-        <header className={styles.header}>
+        <motion.header
+            initial={{
+                translateY: -120,
+            }}
+            animate={{
+                translateY: 0,
+            }}
+            transition={{
+                delay: 0.3,
+                ease: "linear",
+                duration: 1,
+                type: "spring"
+            }}
+            className={styles.header}
+        >
             <section className={styles.header_box}>
                 <div className={styles.logo_box} onClick={() => {
                     window.scrollTo({
@@ -19,18 +62,31 @@ const Header = () => {
                     </h5>
                 </div>
                 <nav className={styles.navigation}>
-                    <Link href="#" className={styles.link}>
-                        Work
-                    </Link>
-                    <Link href="#" className={styles.link}>
-                        About
-                    </Link>
-                    <Link href="#" className={styles.link}>
-                        Contact
-                    </Link>
+                    {navigation.map((nav, index) => (
+                        <Link key={index} href={nav.link} className={styles.link}>
+                            {nav.title}
+                        </Link>
+                    ))}
                 </nav>
+                {width < 839 && (
+                    menuIsOpen ?
+                        <Image src={"/icons/close.svg"} alt="" width={25} height={25} onClick={() => setMenuIsOpen(!menuIsOpen)} />
+                        :
+                        <Image src={"/icons/menu.svg"} alt="" width={30} height={30} onClick={() => setMenuIsOpen(!menuIsOpen)} />
+                )}
+                {width < 839 && menuIsOpen && (
+                    <nav className={styles.mobile_nav}>
+                        {navigation.map((nav, index) => (
+                            <div key={index} className={styles.nav_block}>
+                                <Link href={nav.link} className={styles.link}>
+                                    {nav.title}
+                                </Link>
+                            </div>
+                        ))}
+                    </nav>
+                )}
             </section>
-        </header>
+        </motion.header>
     );
 }
 
