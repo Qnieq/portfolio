b582a105cb76e2164d332b7d1151b288b7df9f58
Mike <misha.o.son@gmail.com>
2024-10-16 22:04:38 +0300

    added analitycs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Montserrat } from "next/font/google";
 import "./globals.scss";
 import BgEffect from "@/components/ui/bg-effect/BgEffect";
 import Header from "@/components/ui/header/Header";
+import { Analytics } from "@vercel/analytics/react"
 
 const montserrat = Montserrat({ subsets: ["latin", "cyrillic"] });
 
@@ -20,6 +21,7 @@ export default function RootLayout({
       <body className={montserrat.className}>
         <BgEffect />
         <Header />
+        <Analytics />
         {children}
       </body>
     </html>
