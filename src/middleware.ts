80906f0614b22f3577638cd41e57a0c5db4cbb96
Mike <misha.o.son@gmail.com>
2024-11-27 20:03:07 +0300

    simple changes

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,18 +2,18 @@ import { NextResponse } from 'next/server';
 
 export function middleware(req: NextResponse) {
     const userAgent = req.headers.get('user-agent') || '';
-  
+
     // Простейшая проверка на мобильное устройство
     const isMobile = /Mobile|Android|iPhone|iPad/i.test(userAgent);
-  
+
     if (isMobile) {
-      return NextResponse.redirect(new URL('/not-allowed', req.url));
+        return NextResponse.redirect(new URL('/not-allowed', req.url));
     }
-  
+
     return NextResponse.next();
-  }
-  
-  // Настройка путей, на которых работает middleware
-  export const config = {
+}
+
+// Настройка путей, на которых работает middleware
+export const config = {
     matcher: ['/', '/not-allowed'], // Укажите пути, где нужно применить middleware
-  };
+};
