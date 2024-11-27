3f21e67f171d3a3e6dbab9ceb7e818e6e6b0dc9e
Mike <misha.o.son@gmail.com>
2024-11-27 21:59:48 +0300

    added hint to 'got to top' button

--- src/components/ui/top_button/TopButton.tsx
+++ src/components/ui/top_button/TopButton.tsx
@@ -1,14 +1,14 @@
-import { ArrowBigUp } from "lucide-react";
+import { ArrowBigUpDash } from "lucide-react";
 import styles from "./TopButton.module.scss"
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
 
 export function TopButton() {
     const [showButton, setShowButton] = useState(false);
+    const [isHovered, setIsHovered] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
-            // Показываем кнопку, если прокрутка больше 100px
             setShowButton(window.scrollY > 100);
         };
 
@@ -24,11 +24,14 @@ export function TopButton() {
             {showButton && (
                 <motion.button
                     className={styles.btn_top}
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
                     onClick={() => {
                         window.scrollTo({ top: 0, behavior: 'smooth' })
                         setTimeout(() => {
                             window.scrollTo({ top: 0, behavior: 'smooth' })
                         }, 110)
+                        setIsHovered(false)
                     }}
                     initial={{ opacity: 0, y: 50 }}
                     animate={{ opacity: 1, y: 0 }}
@@ -38,10 +41,27 @@ export function TopButton() {
                         ease: "leaner",
                         type: "spring",
                         bounce: 0.05,
-                        delay: 0.1,
                     }}
                 >
-                    <ArrowBigUp size={28} color="#fff" className={styles.icon} />
+                    <ArrowBigUpDash size={28} color="#fff" className={styles.icon} />
+                    <AnimatePresence>
+                        {isHovered && (
+                            <motion.h6
+                                className={styles.go_to_top}
+                                initial={{ opacity: 0, y: 50 }}
+                                animate={{ opacity: 1, y: 0 }}
+                                exit={{ opacity: 0, y: 50 }}
+                                transition={{
+                                    duration: 0.4,
+                                    ease: "leaner",
+                                    type: "spring",
+                                    bounce: 0.05,
+                                }}
+                            >
+                                Go to top
+                            </motion.h6>
+                        )}
+                    </AnimatePresence>
                 </motion.button>
             )}
         </AnimatePresence>
