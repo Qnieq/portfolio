d512db482c5b2cca37db8f21b36972dbe8a86248
Mike <misha.o.son@gmail.com>
2024-11-27 15:38:33 +0300

    simple changes

---
+++ src/app/not-allowed/page.tsx
@@ -0,0 +1,12 @@
+import type { Metadata } from 'next';
+import { NotAllowed } from '../not-allowed';
+
+export const metadata: Metadata = {
+    title: 'Not Allowed',
+};
+
+export default function Page() {
+    return <div>
+        <NotAllowed />
+    </div>
+}
