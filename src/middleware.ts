0919d3968163b595c5f384ae2b05affc14e9135c
Mike <misha.o.son@gmail.com>
2025-01-25 14:19:52 +0300

    [fix]: middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,31 +4,30 @@ import { languages, defaultLanguage } from './config/languages'
 
 export function middleware(req: NextRequest) {
     const userAgent = req.headers.get('user-agent') || '';
+    const pathname = req.nextUrl.pathname;
 
-    // Get the pathname of the request (e.g. /, /about, /blog/first-post)
-    const pathname = req.nextUrl.pathname
+    const pathnameLocale = Object.keys(languages).find((locale) =>
+        pathname.startsWith(`/${locale}/`) || pathname === `/${locale}`
+    );
 
-    // Check if the pathname starts with our supported languages
-    const pathnameIsMissingLocale = Object.keys(languages).every(
-        (locale) => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-    )
-
-    // Простейшая проверка на мобильное устройство
     const isMobile = /Mobile|Android|iPhone|iPad/i.test(userAgent);
 
-    if (pathnameIsMissingLocale) {
-        // Redirect if there is no locale
-        const locale = defaultLanguage
-        return NextResponse.redirect(
-            new URL(`/${locale}${pathname}`, req.url)
-        )
-    } else if (isMobile && pathnameIsMissingLocale) {
-        const locale = defaultLanguage
-        return NextResponse.redirect(new URL(`/${locale}/not-allowed`, req.url));
+    if (!pathnameLocale) {
+        const locale = defaultLanguage;
+        return NextResponse.redirect(new URL(`/${locale}${pathname}`, req.url));
     }
+
+    if (isMobile && pathname.endsWith('/not-allowed')) {
+        return NextResponse.next();
+    }
+
+    if (isMobile) {
+        return NextResponse.redirect(new URL(`/${pathnameLocale}/not-allowed`, req.url));
+    }
+
+    return NextResponse.next();
 }
 
-// Настройка путей, на которых работает middleware
 export const config = {
     matcher: ['/((?!_next|api|favicon.ico|logos|icons|images).*)'],
 };
